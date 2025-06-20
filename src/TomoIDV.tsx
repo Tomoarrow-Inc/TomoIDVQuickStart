1ea55151ddb4485842fd2ba8446d50de5bf75d9c
이찬우 <leechanwoo@MacBook-Pro-M3-1T.local>
2025-06-20 19:42:47 +0900

    JSON console out

--- src/TomoIDV.tsx
+++ src/TomoIDV.tsx
@@ -109,7 +109,7 @@ export default function TomoIDVClient({ connection_status, session_id }: TomoIDV
               })
                   .then(res => res.json())
                   .then(data => {
-                      console.log(data);
+                      console.log(JSON.stringify(data, null, 2));
                   })
                   .catch(err => {
                       console.error(err);
