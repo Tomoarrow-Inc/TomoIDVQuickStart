9f248479a70c049068d0626c3f8f06fa7de2b581
이찬우 <leechanwoo@MacBook-Pro-M3-1T.local>
2025-06-20 14:18:48 +0900

    built tomo-idv-client exmaple code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { SessionWebHook, WebhookStatus, Signin, StartTomoIDV } from 'tomo-idv-client';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="flex space-x-4 items-center">
+
+          <SessionWebHook>
+            {(connection_status, session_id) => (
+              <div className="space-y-4">
+                <WebhookStatus session_id={session_id} connectionStatus={connection_status}/>
+                <div className="flex space-x-4 items-center">
+                  <Signin
+                    className="w-full py-2.5 px-4 text-sm font-semibold tracking-wider rounded text-white bg-gray-800 hover:bg-[#222] focus:outline-none"
+                    label='Custom SignIn Label'
+                  />
+                  <StartTomoIDV 
+                      session_id={session_id}
+                      className="w-full py-2.5 px-4 text-sm font-semibold tracking-wider rounded text-white bg-gray-800 hover:bg-[#222] focus:outline-none"
+                      label='Custom StartTomoIDV Label'
+                  />
+                </div>
+              </div>
+              )}
+            </SessionWebHook> 
+        </div>
       </header>
     </div>
   );
